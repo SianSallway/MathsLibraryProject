9eb727c6d0f192b7bf72e3ae908d512394114b74
SianSallway <37866416+SianSallway@users.noreply.github.com>
2019-06-05 20:14:09 +1000

    writing double versions of max, min and clamp functions

--- MathLib/MathFuncs.cpp
+++ MathLib/MathFuncs.cpp
@@ -3,6 +3,14 @@
 #include "MathFuncs.h"
 #include <stdexcept>
 
+MathFuncs::MathFuncs()
+{
+}
+
+MathFuncs::~MathFuncs()
+{
+}
+
 //returns a + b
 double MathFuncs::Add(double a, double b)
 {
@@ -35,7 +43,20 @@ float MathFuncs::Max(float a, float b)
 		return b;
 }
 
+double MathFuncs::Max(double a, double b)
+{
+	if (a > b)
+		return a;
+	else
+		return b;
+}
+
 float MathFuncs::Min(float a, float b)
+{
+	return a < b ? a : b;
+}
+
+double MathFuncs::Min(double a, double b)
 {
 	return a < b ? a : b;
 }
@@ -45,10 +66,7 @@ float MathFuncs::Clamp(float a, float min, float max)
 	return Max(min, Min(a, max));
 }
 
-MathFuncs::MathFuncs()
+double MathFuncs::Clamp(double a, double min, double max)
 {
-}
-
-MathFuncs::~MathFuncs()
-{
+	return Max(min, Min(a, max));
 }
--- MathLib/MathFuncs.h
+++ MathLib/MathFuncs.h
@@ -20,8 +20,14 @@ public:
 
 	static float Max(float a, float b);
 
+	static double Max(double a, double b);
+
 	static float Min(float a, float b);
+	
+	static double Min(double a, double b);
 
 	static float Clamp(float a, float min, float max);
+
+	static double Clamp(double a, double min, double max);
 };
 
